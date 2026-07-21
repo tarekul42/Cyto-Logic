67bbcd3fc8550d0859f1027dbca138da82c9b273
tarekul42 <tarekulrifat205@gmail.com>
2026-07-22 02:12:29 +0600

    Remove unused files

--- frontend/src/test/testUtils.jsx
+++
@@ -1,6 +0,0 @@
-import { ToastProvider } from '../components/Toast'
-
-export function renderWithProviders(ui) {
-  const Wrapper = ({ children }) => <ToastProvider>{children}</ToastProvider>
-  return { Wrapper, ui }
-}
